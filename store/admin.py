9cb02e9fea8f78303d1bbe90d3e7e331773abef9
Mudassar Bagwan <mudassarbagwan26@gmail.com>
2023-11-18 23:49:19 +0530

    change req

--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import Product,Variation,ReviewRating,ProductGallery
-import admin_thumbnails
+
 
 # Register your models here.
-@admin_thumbnails.thumbnail('image')
+
 class ProductGalleryInline(admin.TabularInline):
     model = ProductGallery
     extra = 1

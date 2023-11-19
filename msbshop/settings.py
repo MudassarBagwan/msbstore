5b90cfd53bcdc1c100907aa29676de17b3f5e6a7
Mudassar Bagwan <mudassarbagwan26@gmail.com>
2023-11-19 23:25:57 +0530

    db add

--- msbshop/settings.py
+++ msbshop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =config('SECRET_KEY')
 DEBUG = config('DEBUG',default=True,cast=bool)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['msb-env.eba-tadbfeme.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition

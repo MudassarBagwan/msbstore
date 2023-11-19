75d39f4fd29398b3075302459d1e661182d9f305
Mudassar Bagwan <mudassarbagwan26@gmail.com>
2023-11-19 10:41:49 +0530

    allow host

--- msbshop/settings.py
+++ msbshop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =config('SECRET_KEY')
 DEBUG = config('DEBUG',default=True,cast=bool)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://msbshop-env.eba-9ftycx9c.us-west-2.elasticbeanstalk.com/']
 
 
 # Application definition

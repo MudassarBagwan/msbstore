571afcd90f28e61b34b800ff645bda7144052f0d
Mudassar Bagwan <mudassarbagwan26@gmail.com>
2023-11-20 07:23:29 +0530

    add static

--- msbshop/settings.py
+++ msbshop/settings.py
@@ -158,12 +158,12 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_S3_FILE_OVERWRITE=False
 AWS_DEFAULT_ACL='public-read'    
-AWS_LOCATION='static'
+AWS_LOCATION='static/'
 
 STATTICFILES_DIRS=[
     'msbshop/static',
 ]
-STATIC_URL = 'http://%s/%s/' %(AWS_S3_CUSTOM_DOMAIN,AWS_LOCATION)
+STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN,AWS_LOCATION)
 STATICFILES_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = 'static/'

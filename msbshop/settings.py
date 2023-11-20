c0095ea7c82d4662046495c35809c3da17d5f4aa
Mudassar Bagwan <mudassarbagwan26@gmail.com>
2023-11-20 06:52:47 +0530

    add static

--- msbshop/settings.py
+++ msbshop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =config('SECRET_KEY')
 DEBUG = config('DEBUG',default=True,cast=bool)
 
 
-ALLOWED_HOSTS = ['msb-env.eba-tadbfeme.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['msb-env.eba-tadbfeme.us-west-2.elasticbeanstalk.com','*']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -149,8 +150,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+ # aws settings
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_S3_FILE_OVERWRITE=False
+AWS_DEFAULT_ACL='public-read'    
+AWS_LOCATION='static'
 
+STATTICFILES_DIRS=[
+    'msbshop/static',
+]
+STATIC_URL = 'http://%s/%s/' %(AWS_S3_CUSTOM_DOMAIN,AWS_LOCATION)
+STATICFILES_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
+
+#STATIC_URL = 'static/'
+
+DEFAULT_FILE_STORAGE ='msbshop.media_storages.MediaStorage'
 MEDIA_URL='uploads/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,MEDIA_URL)
